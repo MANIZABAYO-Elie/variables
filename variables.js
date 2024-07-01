070c86f781a32f67771b9a6e86c036580856584c
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-07-01 23:14:16 +0200

    Update variables.js

--- variables.js
+++ variables.js
@@ -6,4 +6,6 @@ console.log(w);
 console.log(z);
 let firstName = "Elie"
 let lastName ="MANIZABAYO"
+let age = 21
 console.log("Good Evening!",firstName,lastName);
+console.log("My name is",firstName,lastName , "I am" ,age ,"years old");
